c7026781708e360890c594372a18d81e36965e9a
MxAziz <hereabdulaziz@gmail.com>
2026-08-15 18:17:08 +0600

    project initialized with react & tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1 className='bg-amber-400 text-4xl'>Currency Converter</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
